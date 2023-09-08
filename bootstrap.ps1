63166909df22b17edd3dd654b511240f2678c21d
brootware <brootware@outlook.com>
2023-09-09 00:09:41 +0800

    added first skeleton bootstrap scripts

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,1 +1,7 @@
-# blup
+choco install vagrant
+choco install python3 --pre 
+python3 -m venv .venv
+.\venv\bin\Activate.ps1
+python3 -m pip install --upgrade pip
+python3 -m pip install ansible-core==2.12.6
+vagrant up
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
+# Get the operating system name
+os_name=$(uname -s)
 
-git clone https://github.com/brootware/kali-ctf.git && cd kali-ctf
-sudo apt install python3-venv
-python3 -m venv .venv
-source .venv/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install ansible-core==2.12.6
+# Check if the operating system is macOS
+if [[ "$os_name" == "Darwin" ]]; then
+    git clone https://github.com/brootware/kali-ctf.git && cd kali-ctf
+    brew install python3
+    python3 -m pip install pipx
+    pipx ensurepath
+    pipx install python3-venv
+    python3 -m venv .venv
+    source .venv/bin/activate
+    python3 -m pip install --upgrade pip
+    python3 -m pip install ansible-core==2.12.6
+    vagrant up
+# Check if the operating system is Linux
+elif [[ "$os_name" == "Linux" ]]; then
+    git clone https://github.com/brootware/kali-ctf.git && cd kali-ctf
+    sudo apt install python3-venv
+    python3 -m venv .venv
+    source .venv/bin/activate
+    python3 -m pip install --upgrade pip
+    python3 -m pip install ansible-core==2.12.6
+    vagrant up
+else
+    echo "Unknown operating system."
+fi
 
 
 
